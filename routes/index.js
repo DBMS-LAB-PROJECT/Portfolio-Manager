0890b25f94d73f116357df57fe1d4cb0b3bc1578
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-04-26 18:25:09 +0530

    fix the login and signup issue

--- routes/index.js
+++ routes/index.js
@@ -34,13 +34,14 @@ const con = mysql2.createConnection({
 function isloggedin(req, res, next) {
     // console.log("islogged in is running")
     // console.log(req.user);
-    req.user ? next() : res.sendStatus(401);
+    req.user ? next() : res.redirect('/login');
 }
 
 database.query("use portfolio_manager");
 
 // ****************************     GET ROUTES    ********************************************************
 router.get("/", function (req, res) {
+    console.log("home");
     res.render("home");
 })
 
@@ -79,8 +80,8 @@ router.get("/auth/failure", function (req, res) {
 })
 
 router.get("/dashboard", isloggedin, function (req, res) {
-    
-    res.render("dashboard", {username: req.user.user_name});
+
+    res.render("dashboard", { username: req.user.user_name });
 });
 
 router.get("/logout", function (req, res) {
@@ -93,58 +94,58 @@ router.get("/terms_and_conditions", function (req, res) {
     res.render("terms_and_conditions");
 });
 
-router.get("/liabilities", isloggedin, function(req, res){
+router.get("/liabilities", isloggedin, function (req, res) {
 
     let sql = "SELECT * FROM ((liability_amounts a INNER JOIN liability_interests b ON a.user_id = b.user_id) INNER JOIN liability_interest_rates c ON a.user_id = c.user_id) WHERE a.user_id  = ?";
 
-    database.query(sql, req.user.user_id,function(err, result, fields){
-        
-        let total_amount = result[0].car_loan_amount; 
-        total_amount+= result[0].property_loan_amount;
-        total_amount+= result[0].educational_loan_amount;
-        total_amount+= result[0].home_loan_amount;
-        total_amount+= result[0].bills_payable_amount;
-        total_amount+= result[0].mortgage_payable_amount;
-        total_amount+= result[0].capital_leases_amount;
-        total_amount+= result[0].bank_account_overdrafts_amount;
-        
-        let total_interest = result[0].car_loan_interest; 
-        total_interest+= result[0].property_loan_interest;
-        total_interest+= result[0].educational_loan_interest;
-        total_interest+= result[0].home_loan_interest;
-        total_interest+= result[0].bills_payable_interest;
-        total_interest+= result[0].mortgage_payable_interest;
-        total_interest+= result[0].capital_leases_interest;
-        total_interest+= result[0].bank_account_overdrafts_interest;
+    database.query(sql, req.user.user_id, function (err, result, fields) {
+
+        let total_amount = result[0].car_loan_amount;
+        total_amount += result[0].property_loan_amount;
+        total_amount += result[0].educational_loan_amount;
+        total_amount += result[0].home_loan_amount;
+        total_amount += result[0].bills_payable_amount;
+        total_amount += result[0].mortgage_payable_amount;
+        total_amount += result[0].capital_leases_amount;
+        total_amount += result[0].bank_account_overdrafts_amount;
+
+        let total_interest = result[0].car_loan_interest;
+        total_interest += result[0].property_loan_interest;
+        total_interest += result[0].educational_loan_interest;
+        total_interest += result[0].home_loan_interest;
+        total_interest += result[0].bills_payable_interest;
+        total_interest += result[0].mortgage_payable_interest;
+        total_interest += result[0].capital_leases_interest;
+        total_interest += result[0].bank_account_overdrafts_interest;
 
         if (err) throw err;
-        res.render("liabilities", { liabilities: result, 
-                                    total_amount: total_amount, 
-                                    total_interest: total_interest 
-                                });
+        res.render("liabilities", {
+            liabilities: result,
+            total_amount: total_amount,
+            total_interest: total_interest
+        });
 
     });
-    
-});
- 
-router.get("/liabilities/edit/:liability_type", isloggedin, function(req, res, next){
 
+});
+
+router.get("/liabilities/edit/:liability_type", isloggedin, function (req, res, next) {
     let type = req.params.liability_type;
     let liability_type = _.snakeCase(_.lowerCase(type));
-    let amount = liability_type+"_amount";
-    let interest = liability_type+"_interest";
-    let rate = liability_type+"_rate";
+    let amount = liability_type + "_amount";
+    let interest = liability_type + "_interest";
+    let rate = liability_type + "_rate";
 
-    let sql = "SELECT a."+amount+", b."+interest+", c."+rate+" FROM ((liability_amounts a INNER JOIN liability_interests b ON a.user_id = b.user_id) INNER JOIN liability_interest_rates c ON a.user_id = c.user_id) WHERE a.user_id = ?";
+    let sql = "SELECT a." + amount + ", b." + interest + ", c." + rate + " FROM ((liability_amounts a INNER JOIN liability_interests b ON a.user_id = b.user_id) INNER JOIN liability_interest_rates c ON a.user_id = c.user_id) WHERE a.user_id = ?";
 
-    database.query(sql, req.user.user_id, function(err, result){
+    database.query(sql, req.user.user_id, function (err, result) {
 
-        if(err) throw err;
+        if (err) throw err;
         console.log(result);
         res.render("edit_liability", { liability_name: type, liability: result[0], amount: amount, rate: rate });
 
     });
-    
+
 });
 
 
@@ -170,22 +171,22 @@ router.post("/signup", passport.authenticate('local-signup', {
 )
 
 
-router.post("/liabilities/edit/:liability_type", function(req, res){
+router.post("/liabilities/edit/:liability_type", function (req, res) {
 
     let type = req.params.liability_type;
     let liability_type = _.snakeCase(_.lowerCase(type));
-    let amount = liability_type+"_amount";
-    let interest = liability_type+"_interest";
-    let rate = liability_type+"_rate";
+    let amount = liability_type + "_amount";
+    let interest = liability_type + "_interest";
+    let rate = liability_type + "_rate";
 
     let r = req.body.interest_rate;
     let t = req.body.time_period;
     let p = req.body.amount;
     let int = ((p * r * t) / 100);
 
-    let sql = "UPDATE liability_amounts a, liability_interests b, liability_interest_rates c SET a."+amount+" = ?, b."+interest+" = ?, c."+rate+" = ? WHERE a.user_id = b.user_id AND b.user_id = c.user_id AND a.user_id = ?";
+    let sql = "UPDATE liability_amounts a, liability_interests b, liability_interest_rates c SET a." + amount + " = ?, b." + interest + " = ?, c." + rate + " = ? WHERE a.user_id = b.user_id AND b.user_id = c.user_id AND a.user_id = ?";
 
-    database.query(sql, [p, int, r, req.user.user_id], function(err, result){
+    database.query(sql, [p, int, r, req.user.user_id], function (err, result) {
 
         if (err) throw err;
         console.log(result);
@@ -200,7 +201,7 @@ router.get("/stocks", async function (req, res) {
     con.query("use portfolio_manager");
     // const user = req.user;
     // console.log("this is get test route------->" + user);
-    res.render("stocks");
+    res.render("stocks2");
 })
 
 router.post("/stocks", async function (req, res) {
@@ -216,97 +217,188 @@ router.post("/stocks", async function (req, res) {
     // const logo_json = await logo_response.json();
     // const logo = logo_json.url; 
     res.render("company", { logo: "", name: name, exchange: jsondata.meta.exchange, symbol: symbol });
+    // res.render("company", { logo: "", name: 'Apple Inc', exchange: 'NASDAQ', symbol: 'AAPL' });
+    // res.render("company");
     // res.send(jsondata);
 })
 
-router.post("/add", function (req, res) {
+router.post("/stocks/add", function (req, res) {
+    con.query("use portfolio_manager");
+    // const user_id = req.user;
+    const user_id = '113720373204677842542';
+    // console.log(user_id);
+    let companyName = req.body.companyName;
+    console.log(companyName);
+    let symbol = req.body.symbol;
+    console.log(symbol);
+    const quantity = req.body.quantity;
+    console.log(quantity);
+    const pricePerShare = req.body.pricePerShare;
+    console.log(pricePerShare);
+    const dateOfBuying = req.body.dateOfBuying;
+    console.log(dateOfBuying);
+    // console.log("this is post route------->" + user_id); 
+
+    let sql = 'insert into stocks (user_id, quantity, symbol, price, buyDate, companyName) values(?,?,?,?,?,?)'
+    con.query(sql, [user_id, quantity, symbol, pricePerShare, dateOfBuying, companyName], function (err) {
+        if (err) console.log(err);
+        else console.log('1 stock inserted');
+    })
+})
+
+// router.post("/alreadyHaveSelectedStockButtonAdd", async function (req, res) {
+//     con.query("use portfolio_manager");
+//     const user_id = '113720373204677842542';
+//     // const user_id = req.user;
+//     let symbol = req.body.symbol;
+//     // console.log(symbol);
+//     const quantity = parseInt(req.body.quantity);
+//     // console.log('quantity---->' + quantity);
+//     const prevQuantity = parseInt(req.body.prevQuantity);
+//     let sql = 'select * from stocks where symbol = ? and user_id = ?'
+
+//     const newQuantity = prevQuantity + quantity;
+//     // console.log('newQuantity---->' + newQuantity);
+//     sql = 'update stocks set quantity = ? where symbol = ? and user_id = ?';
+//     con.query(sql, [newQuantity, symbol, user_id], function (err) {
+//         if (err) console.log(err);
+//     })
+//     res.sendStatus(200);
+// })
+
+// router.post("/alreadyHaveSelectedStockButtonUpdate", function (req, res) {
+//     con.query("use portfolio_manager");
+//     const user_id = '113720373204677842542';
+//     // const user_id = req.user;
+//     let symbol = req.body.symbol;
+//     // console.log(symbol);
+//     const quantity = req.body.quantity;
+//     // console.log('quantity---->' + quantity);
+//     let sql = 'select * from stocks where symbol = ? and user_id = ?'
+//     const newQuantity = parseInt(quantity);
+//     // console.log(newQuantity);
+
+//     sql = 'update stocks set quantity = ? where symbol = ? and user_id = ?';
+//     con.query(sql, [newQuantity, symbol, user_id], function (err) {
+//         if (err) console.log(err);
+//     })
+//     res.sendStatus(200);
+// })
+
+router.post('/currentHoldingModal', function (req, res) {
     con.query("use portfolio_manager");
     // const user_id = req.user;
     const user_id = '113720373204677842542';
     // console.log(user_id);
     let symbol = req.body.symbol;
     // console.log(symbol);
-    const quantity = req.body.quantity;
-    // console.log(quantity);
     // console.log("this is post route------->" + user_id); 
-    let sql = 'select * from stocks where symbol = ? and user_id = ?'
+    let sql = 'select stock_id,quantity,symbol,price,buyDate from stocks where symbol = ? and user_id = ?'
     con.query(sql, [symbol, user_id], function (err, rows) {
         if (err) console.log(err);
-        if (rows.length === 0) {
-            let sql = 'insert into stocks (user_id, quantity, symbol) values(?,?,?)'
-            con.query(sql, [user_id, quantity, symbol], function (err) {
-                if (err) console.log(err);
-                else console.log('1 stock inserted');
-            })
-
-            res.send({ prevQuantity: 0 });
+        // console.log(rows);
+        if (rows.length == 0) {
+            res.send(rows);
         }
         else {
-            const prevQuantity = rows[0].quantity;
-            res.send({ prevQuantity: prevQuantity });
+            res.send(rows);
         }
     })
 })
 
-router.post("/alreadyHaveSelectedStockButtonAdd", async function (req, res) {
-    con.query("use portfolio_manager");
-    const user_id = '113720373204677842542';
-    // const user_id = req.user;
-    let symbol = req.body.symbol;
-    // console.log(symbol);
-    const quantity = parseInt(req.body.quantity);
-    // console.log('quantity---->' + quantity);
-    const prevQuantity = parseInt(req.body.prevQuantity);
-    let sql = 'select * from stocks where symbol = ? and user_id = ?'
-
-    const newQuantity = prevQuantity + quantity;
-    // console.log('newQuantity---->' + newQuantity);
-    sql = 'update stocks set quantity = ? where symbol = ? and user_id = ?';
-    con.query(sql, [newQuantity, symbol, user_id], function (err) {
-        if (err) console.log(err);
-    })
-    res.sendStatus(200);
-})
-
-router.post("/alreadyHaveSelectedStockButtonUpdate", function (req, res) {
-    con.query("use portfolio_manager");
-    const user_id = '113720373204677842542';
-    // const user_id = req.user;
-    let symbol = req.body.symbol;
-    // console.log(symbol);
-    const quantity = req.body.quantity;
-    // console.log('quantity---->' + quantity);
-    let sql = 'select * from stocks where symbol = ? and user_id = ?'
-    const newQuantity = parseInt(quantity);
-    // console.log(newQuantity);
-
-    sql = 'update stocks set quantity = ? where symbol = ? and user_id = ?';
-    con.query(sql, [newQuantity, symbol, user_id], function (err) {
-        if (err) console.log(err);
-    })
-    res.sendStatus(200);
-})
-
-router.post('/currentHolding', function (req, res) {
+router.post('/currentHoldingModal/remove', function (req, res) {
     con.query("use portfolio_manager");
     // const user_id = req.user;
     const user_id = '113720373204677842542';
     // console.log(user_id);
     let symbol = req.body.symbol;
+    let stock_id = req.body.stock_id;
     // console.log(symbol);
     // console.log("this is post route------->" + user_id); 
-    let sql = 'select * from stocks where symbol = ? and user_id = ?'
-    con.query(sql, [symbol, user_id], function (err, rows) {
+    let sql = 'delete from stocks where symbol = ? and user_id = ? and stock_id = ?'
+    con.query(sql, [symbol, user_id, stock_id], function (err, rows) {
         if (err) console.log(err);
-        if(rows.length == 0){
-            res.send({ prevQuantity: 0 });
+        // console.log(rows);
+        res.sendStatus(200);
+    })
+})
+
+router.get('/about', (req, res) => {
+    res.render('about');
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+router.get('/currentHoldings', (req, res) => {
+    con.query('use portfolio_manager');
+    // const user_id = req.user;
+    const user_id = '113720373204677842542';
+    let userName;
+    let sql = 'select user_name from login_credentials where user_id = ?';
+    con.query(sql, user_id, function(err, rows){
+        if(err) console.log(err);
+        // console.log(rows);
+        res.render('currentHoldings', {userName : rows[0].user_name});
+        // userName = rows[0].user_name;
+        console.log(userName);
+    })
+})
+
+router.post('/currentHoldings', function (req, res) {
+    con.query('use portfolio_manager');
+    // const user_id = req.user;
+    const user_id = '113720373204677842542';
+    // console.log("this is post route------->" + user_id); 
+    let sql = 'select stock_id,quantity,symbol,price,buyDate,companyName from stocks where user_id = ?'
+    con.query(sql, user_id, function (err, rows) {
+        if (err) console.log(err);
+        // console.log(rows);
+        if (rows.length == 0) {
+            res.send(rows);
         }
-        else{
-            res.send({prevQuantity: rows[0].quantity})
+        else {
+            res.send(rows);
         }
     })
 })
 
+router.post('/currentHoldings/remove', function (req, res) {
+    con.query("use portfolio_manager");
+    // const user_id = req.user;
+    const user_id = '113720373204677842542';
+    // console.log(user_id);
+    // let symbol = req.body.symbol;
+    let stock_id = req.body.stock_id;
+    // console.log(symbol);
+    // console.log("this is post route------->" + user_id); 
+    let sql = 'delete from stocks where user_id = ? and stock_id = ?'
+    con.query(sql, [user_id, stock_id], function (err, rows) {
+        if (err) console.log(err);
+        // console.log(rows);
+        res.sendStatus(200);
+    })
+})
 
+router.post('/currentHoldings/search', (req, res) => {
+    con.query("use portfolio_manager");
+    // const user_id = req.user;
+    const user_id = '113720373204677842542';
+    const searchItem = req.body.searchItem;
+    // console.log(searchItem);
+    // console.log("this is post route------->" + user_id); 
+    let sql = "select stock_id,quantity,symbol,price,buyDate,companyName from stocks where symbol   like '%" + searchItem + "%' or companyName like '%" + searchItem + "%' and user_id = '" + user_id + "'"; 
+    con.query(sql, [searchItem, searchItem, user_id], function (err, rows) {
+        if (err) console.log(err);
+        // console.log(rows);
+        if (rows.length == 0) {
+            res.send(rows);
+        }
+        else {
+            res.send(rows);
+        }
+    })
+})
 
 module.exports = router;
