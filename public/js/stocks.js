abbaa99d65193ef26ae2f2eb7807c382be240a08
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-04-26 18:23:59 +0530

    stock.js is modified  and almost completed

--- public/js/stocks.js
+++ public/js/stocks.js
@@ -14,72 +14,83 @@ const getpost = async () => {
     }
     obj_array = await obj_data.data;
 
-    for (let i = 0; i < 50; i++) {
-        const htmlData = `
-        <form action="/stocks" method="post">
-                <input type="hidden" value="${obj_array[index + i].symbol}" name="company_symbol">
-                <button><span class="symbol">
-                ${obj_array[index + i].symbol}
-                    </span></button>
+    // for (let i = 0; i < 50; i++) {
+    //     const htmlData = `
+    //     <form action="/stocks" method="post">
+    //             <input type="hidden" value="${obj_array[index + i].symbol}" name="company_symbol">
+    //             <button><span class="symbol">
+    //             ${obj_array[index + i].symbol}
+    //                 </span></button>
 
-                <input type="hidden" value="${obj_array[index + i].name}" name="company_name">
-                <span>
-                ${obj_array[index + i].name}
-                </span>
-                <span>
-                ${obj_array[index + i].currency}
-                </span>
-                <span>
-                ${obj_array[index + i].exchange}
-                </span>
-                <span>
-                ${obj_array[index + i].country}
-                <span>
-                ${obj_array[index + i].type}
-                </span>
-                <br>
-            </form>`;
-        container.insertAdjacentHTML("beforeend", htmlData);
-    }
-    loader.classList.add("hide");
+    //             <input type="hidden" value="${obj_array[index + i].name}" name="company_name">
+    //             <span>
+    //             ${obj_array[index + i].name}
+    //             </span>
+    //             <span>
+    //             ${obj_array[index + i].currency}
+    //             </span>
+    //             <span>
+    //             ${obj_array[index + i].exchange}
+    //             </span>
+    //             <span>
+    //             ${obj_array[index + i].country}
+    //             <span>
+    //             ${obj_array[index + i].type}
+    //             </span>
+    //             <br>
+    //         </form>`;
+    //     container.insertAdjacentHTML("beforeend", htmlData);
+    // }
+    // loader.classList.add("hide");
 
 }
 
 getpost();
 
-const showdata = () => {
-    loader.classList.remove("hide");
-    setTimeout(() => {
-        index += 50;
-        getpost();
-    }, 500)
-}
+// const showdata = () => {
+//     loader.classList.remove("hide");
+//     setTimeout(() => {
+//         index += 50;
+//         getpost();
+//     }, 500)
+// }
 
-window.addEventListener('scroll', () => {
-    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
-    if (clientHeight + scrollTop >= scrollHeight) {
-        console.log(index);
-        showdata();
-    }
-})
+// window.addEventListener('scroll', () => {
+//     const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+//     if (clientHeight + scrollTop >= scrollHeight) {
+//         console.log(index);
+//         showdata();
+//     }
+// })
 
 const searchbar = document.getElementById("searchbar");
+searchbar.value="";
 const search_list = document.getElementById("search_list");
+
+const searchResultDiv = document.getElementById('searchResultDiv');
+searchResultDiv.style.display = "none";
+let symbol_arr = [];
+let name_arr = [];
 
 searchbar.addEventListener("keyup", (e) => {
     const query = e.target.value.toLowerCase();
     // console.log(query);
-    const symbol_arr = [];
-    const name_arr = [];
+    symbol_arr = [];
+    name_arr = [];
     obj_array.forEach(company => {
         if (company.symbol.toLowerCase().includes(query) || company.name.toLowerCase().includes(query)) {
-            console.log(company.symbol + "\t\t" + company.name);
+            // console.log(company.symbol + "\t\t" + company.name);
             symbol_arr.push(company.symbol);
             name_arr.push(company.name);
         }
     });
+    // console.log(symbol_arr.length);
+    if(symbol_arr.length == 0){
+        searchResultDiv.style.display = "none";
+    }
+
     let html = ''
-    let n = 10;
+    let n = 4;
     if(n > symbol_arr.length){
         n = symbol_arr.length 
     }
@@ -87,7 +98,10 @@ searchbar.addEventListener("keyup", (e) => {
         html += `<form action="/stocks" method="post">
                 <input type="hidden" value="${symbol_arr[i]}" name="company_symbol">
                 <input type="hidden" value="${name_arr[i]}" name="company_name">
-                <button type="submit"><div>${symbol_arr[i]} ${name_arr[i]}</div></button>
+                <button type="submit" class="resultList">
+                    <div class='companyName'>${name_arr[i]}</div>
+                    <div class='symbol'>${symbol_arr[i]}</div>
+                </button>
                 </form>`
     }
     search_list.innerHTML = html;
@@ -96,4 +110,12 @@ searchbar.addEventListener("keyup", (e) => {
     }
 })
 
+setInterval(() => {
+    if(searchbar.value.length == 0 || symbol_arr.length == 0){
+        searchResultDiv.style.display = "none";
+    }
+    else{
+        searchResultDiv.style.display = "";
+    }
+}, 0);
 
