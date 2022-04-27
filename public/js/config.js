597cfd46bc6f36319887ac42205c7f00c60a7541
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-04-27 12:06:10 +0530

    config.js is removed from girignore

---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+const config = {
+    api: "a7d873a9ef674f3991558d933153c0c9",
+    polygonAPI1: 'yHqZBhn71qNkjDcHuOJpmxzCtt3WUfMt',
+    polygonAPI2: 'TxRI7qSvrjbMNtn5n6A3GWIBEQp0HdpR',
+    polygonAPI3: 'cucRDpRYJeWO_PVaZOl7I40ZMxgw6MYU',
+    polygonAPI4: 'eakt2Ewuma8hxDnW_Ufmy4HpokmrgONU',
+
+}
