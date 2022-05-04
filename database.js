da456ed6fbecceb427eb342b99789b24c046ac19
Saharsh <saharshsranu@gmail.com>
2022-05-04 15:21:57 +0530

    saharsh profile page commit

--- database.js
+++ database.js
@@ -14,3 +14,27 @@ con.connect(function (err) {
 });
 
 module.exports = con;
+
+// Saharsh Database Connection Begin
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: '127.0.0.1',               // Replace with your host name
+  user: 'root',                    // Replace with your database username
+  password: 'password',            // Replace with your database password
+  database: 'profile_page_schema', // Replace with your database Name
+});
+
+connection.connect(function(error){
+	if(error)
+	{
+		throw error;
+	}
+	else
+	{
+		console.log('MySQL Database is connected Successfully');
+	}
+});
+
+module.exports = connection;
+// Saharsh Database Connection End
--- routes/index.js
+++ routes/index.js
@@ -581,4 +581,55 @@ router.get('/dashboard2', (req, res) => {
     res.render('dashboard2');
 })
 
+// Saharsh Code Begin
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var sampledataRouter = require('./routes/sample_data');
+
+router.use(logger('dev'));
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(cookieParser());
+router.use(express.static(path.join(__dirname, 'public')));
+
+router.use('/', sampledataRouter);
+
+// catch 404 and forward to error handler
+router.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+// error handler
+router.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+// render the error page
+res.status(err.status || 500);
+res.render('error');
+});
+
+router.get("/", function(request, response, next){
+
+	var query = "SELECT * FROM user_details WHERE user_id = 4";
+// Put in your database' user_id in the where clause, for indivisual user.
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error; 
+		}
+		else
+		{
+			response.render('sample_data', {title:'USER DETAILS', action:'list', sampleData:data});
+		}
+
+	});
+
+});
+// Saharsh Code End
+
 module.exports = router;
